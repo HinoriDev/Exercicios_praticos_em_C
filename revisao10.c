da1f12de2d0a751f3b9f8ff62bdccc248a6f6eca
Guilherme Hinori <gui-benetti@hotmail.com>
2025-10-17 11:48:08 +0000

    revisao do exercicios 10

--- revisao10.c
+++ revisao10.c
@@ -2,6 +2,26 @@
 int main(){
     int vetor[5];
     
+    for(int pos = 0; pos < 5; pos++){
+        printf("Digite um número inteiro [%d / 5]: ", pos + 1);
+        scanf("%d", &vetor[pos]);
+    }
 
+    for(int pos = 0; pos < 4; pos++){
+        if(vetor[pos] > vetor[pos+1]){
+            int temp = vetor[pos];
+            vetor[pos] = vetor[pos+1];
+            vetor[pos+1] = temp;
+            pos = -1;
+        }
+    }
+    
+    for(int pos = 0;pos < 5; pos++){
+        if(pos == 4) {
+            printf("\n");
+        } else {
+        printf(", ");               
+        }     
+    }
     return 0;
 }
